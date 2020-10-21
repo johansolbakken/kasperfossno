8fd1f5f1b2416b67689e40ce0a46bb6fc337ac85
Adne <aadnegj@stud.ntnu.no>
2020-10-21 15:47:23 +0200

    oppdatert form

--- js/form_validasjon.js
+++ js/form_validasjon.js
@@ -1,5 +1,6 @@
 let button = document.getElementById("button")
+let form = document.getElementById("kontaktskjema")
 
-button.addEventListener("click", () => {
+form.addEventListener("submit", () => {
     alert("Meldingen ble ikke sendt da siden er under konstruksjon")
 })
